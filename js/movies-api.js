f65bf0e4607b4af6471fa479689d31302c6b4553
Chris Hilton <hilton.chris210@gmail.com>
2023-02-02 16:47:55 -0600

    functional view add delete by id

--- js/movies-api.js
+++ js/movies-api.js
@@ -47,3 +47,20 @@ const addMovie = async (movie) => {
 
 // Here is where you will create your own functions to further interact with the database.
 // HAPPY CODING!!!
+
+const deleteMovie = async (movie) => {
+    try {
+        const url = `/movies/${movie.id}`;
+        const options = {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        };
+        let response = await db.fetch(url, options);
+        return await response.json();
+        // console.log("hey this works")
+    } catch (e) {
+        console.error(e);
+    }
+}
