122a821868af270264aa33247b030f1f11686707
Chase Medford <chasemedford1@gmail.com>
2023-02-06 17:29:10 -0600

    Working on Search Function

--- js/index.js
+++ js/index.js
@@ -68,8 +68,6 @@
                 };
                 await addMovie(movie);
                 await loadMovies();
-
-            // $('#movies').html('<div class="movie">');
             });
         }
 
@@ -93,67 +91,11 @@
                     console.log("movie.id =>", movie.id);
                     await deleteMovie(movie);
                     await loadMovies();
-
-                    // $('#movies').html('<div class="movie">');
-
                     }
                 }
             });
         }
 
-
-    //todo write a search function that searches my title and returns that
-    //
-    //
-    //
-    // $('.search-movie-btn').click(function() {
-    //
-    //         for (let i = 0; i < movies.length; i++) {
-    //             if (movies[i].title === $('.search-movie').val()) {
-    //                 let movie = {}
-    //                 movie.id = movies[i].id
-    //                 getMovie(movie);
-    //                 searchhtml = ``;
-    //                 searchhtml += `<div class="movie">
-    //                 <div class="wrapper">
-    //                      <div class="main_card">
-    //                         <div class="card_left">
-    //                             <div class="card_datails">
-    //                                 <h1>${movies[i].title}</h1>
-    //                                 <div class="card_cat">
-    //                                     <p class="year">Year: ${movies[i].year}</p>
-    //                                     <p class="genre">Genre: ${movies[i].genre}</p>
-    //                                     <p class="time">Minutes: ${movies[i].runtime}</p>
-    //                                     <p class="director">Director: ${movies[i].director}</p>
-    //                                     <p class="actors">Actors: ${movies[i].actors}</p>
-    //                                 </div>
-    //                                 <img src="${movies[i].poster}" alt="poster" class="poster">
-    //                                 <div class="social-btn">
-    //                                  <!-- WATCH TRAILER-->
-    //                                    <button>
-    //                                        <a href="${movies[i].trailer}" target="_blank">Watch Trailer</a>
-    //                                    </button>
-    //                                 </div>
-    //                             </div>
-    //                         </div>
-    //                     <div class="card_right">
-    //                         <div class="img_container">
-    //                             <img src="${movies[i].poster}" alt="movie">
-    //                        </div>
-    //                    <div class="play_btn">
-    //                    <a href="https://www.imdb.com/title/tt4912910/" target="_blank">
-    //                    </a>
-    //                    </div>
-    //                    </div>
-    //                 </div>
-    //             </div>
-    //           </div>`
-    //             }
-    //         }
-    //     $('#searched-movie').html(searchhtml)
-    // });
-
-    // todo write a function that updates a movie by searching the title
 
 
 
@@ -192,12 +134,14 @@
         $('#add').show();
         $('#delete').hide();
         $('#update').hide();
+        $('#search').hide();
     });
 
     $('#deleteNav').click(function () {
         $('#delete').show();
         $('#add').hide();
         $('#update').hide();
+        $('#search').hide();
     });
 
     $('#updateNav').click(function () {
@@ -205,10 +149,46 @@
         $('#updateField').show();
         $('#add').hide();
         $('#delete').hide();
+
     });
 
-    $('#home').click(function () {
-        location.reload();
+    $('#homeNav').click(function () {
+        $('#add').hide();
+        $('#delete').hide();
+        $('#update').hide();
+        $('#search').hide();
+    });
+
+    $('#searchNav').click(function () {
+        $('#search').show();
+        $('#add').hide();
+        $('#delete').hide();
+        $('#update').hide();
+    });
+
+
+    //search function
+    $('#search-movie-btn').click(async function () {
+        const movies = await getMovies();
+        console.log("search btn clicked");
+        for (let i = 0; i < movies.length; i++) {
+            console.log(movies[i])
+            if (movies[i].title === $('#search-movie').val()) {
+                let movie = {
+                    title: $('#title1').val(),
+                    rating: $('#rating1').val(),
+                    id: movies[i].id,
+                    year: $('#year1').val(),
+                    director: $('#director1').val(),
+                    runtime: $('#runtime1').val(),
+                    genre: $('#genre1').val(),
+                    actors: $('#actors1').val(),
+                };
+                console.log(movie)
+                await updateMovie(movie);
+                await loadMovies();
+            }
+        }
     });
 
 
@@ -218,14 +198,14 @@
 window.onscroll = function() {stickyBar()};
 
 // Get the navbar
-var navbar = document.getElementById("navbar");
+let navbar = document.querySelector("#navbar");
 
 // Get the offset position of the navbar
-var sticky = navbar.offsetTop;
+let sticky = navbar.offsetTop;
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function stickyBar() {
-    if (window.pageYOffset >= sticky) {
+    if (window.scrollY >= sticky) {
         navbar.classList.add("sticky")
     } else {
         navbar.classList.remove("sticky");
